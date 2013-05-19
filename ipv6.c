867eea6eeaf00332ebc3314502c67b21c9ac1d3e
kumaran127 <kumaran.4353@gmail.com>
2013-05-19 19:13:31 +0530

    IPv6 hop-by-hop extention header writing is added. Woriking.

--- ipv6.c
+++ ipv6.c
@@ -14,10 +14,168 @@ struct ipv6_packet {
 	uint8_t dst[16];
 };
 
-char* pgen_ipv6_hdr_writer(struct packet_data *sp_pd, char *cp_cur) {
+struct hbh_packet {
+	uint8_t nxt_hdr;
+	uint8_t ext_len;
+	char data;
+};
+
+int hbh_option_writer(char *buff, char *value) {
+	uint8_t byte;
+	uint32_t len = 0;
+
+	if ((buff == NULL) || (value == NULL))
+		goto err;
+
+	if (*value++ != '0' && (*value != 'x' || *value != 'X'))
+		goto err;
+	value++;
+
+	byte = 0;
+	while (*value != '\0') {
+		len++;
+		if (*value >= '0' && *value <= '9')
+			byte = byte * 16 + *value - '0';
+		else if (*value >= 'a' && *value <= 'f')
+			byte = byte * 16 + *value - 'a';
+		else if (*value >= 'A' && *value <= 'F')
+			byte = byte * 16 + *value - 'A';
+		else
+			goto err;
+
+		if (len % 2 == 0) {
+			*buff++ = byte;
+			byte = 0;
+		}
+
+		value++;
+	}
+
+	if (len % 2 != 0)
+		goto err;
+	else
+		return len/2;
+
+err:
+	return -1;
+}
+
+char* ipv6_hbh_writer(FILE *fp, char *cp_cur) {
+	struct hbh_packet *pkt = (struct hbh_packet *)cp_cur;
+	char option[MAX_OPTION_LEN], value [MAX_VALUE_LEN];
+	int items = 3, tmp, len = 0;
+
+	while (items--) {
+		if (pgen_parse_option(fp, option, value))
+			goto err;
+
+		if (!strcmp(option, "HBH_NXT_HDR")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->nxt_hdr = (uint8_t)tmp;
+		}
+		else if (!strcmp(option, "HBH_HDR_EXT_LEN")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->ext_len = (uint8_t)tmp;
+		}
+		else if (!strcmp(option, "HBH_OPTION")) {
+			len = hbh_option_writer(&pkt->data, value);
+			if (len < 0)
+				goto err;
+		}
+		else
+			goto err;
+	}
+	return (cp_cur + len + 2);
+
+err:
+	return NULL;
+}
+
+char* pgen_ipv6_writer(FILE *fp, char *cp_cur) {
 	struct ipv6_packet *pkt = (struct ipv6_packet *)cp_cur;
+	char option[MAX_OPTION_LEN], value[MAX_VALUE_LEN];
+	/* 
+	 * Totally we have 9 items in IPv6 header.
+	 * 8 items for packet.
+	 * 1 is the number of extention headers
+	 */
+	int hdr_items = 9, tmp, ext_hdrs;
 
 	pkt->ver_tc_fw = 0;
+
+	while (hdr_items--) {
+		if (pgen_parse_option(fp, option, value))
+			goto err;
+
+		if (!strcmp(option, "IPV6_VERSION")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->ver_tc_fw |= tmp << 28;
+		}
+		else if (!strcmp(option, "IPV6_TRAFFIC_CLASS")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->ver_tc_fw |= tmp << 20;
+		}
+		else if (!strcmp(option, "IPV6_FLOW_LABEL")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->ver_tc_fw |= tmp;
+		}
+		else if (!strcmp(option, "IPV6_PAYLOAD_LENGTH")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->payload_length = htons(tmp);
+		}
+		else if (!strcmp(option, "IPV6_NEXT_HEADER")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->next_header = (uint8_t)tmp;
+		}
+		else if (!strcmp(option, "IPV6_HOP_LIMIT")) {
+			if (pgen_store_dec(&tmp, value))
+				goto err;
+			pkt->hop_limit = (uint8_t)tmp;
+		}
+		else if (!strcmp(option, "IPV6_SRC_ADDR")) {
+			if (ip6_writer(pkt->src, value))
+				goto err;
+		}
+		else if (!strcmp(option, "IPV6_DST_ADDR")) {
+			if (ip6_writer(pkt->dst, value))
+				goto err;
+		}
+		else if (!strcmp(option, "IPV6_EXT_HDRS")) {
+			if (pgen_store_dec(&ext_hdrs, value))
+				goto err;
+		}
+		else {
+			fprintf(stderr, "Unknown IPv6 option\n");
+			goto err;
+		}
+	}
+	pkt->ver_tc_fw = htonl(pkt->ver_tc_fw);
+	cp_cur = cp_cur + sizeof(struct ipv6_packet);
+
+	/* Writing IPv6 extention headers */
+	while (ext_hdrs--) {
+		if (pgen_parse_option(fp, option, value))
+			goto err;
+
+		if (!strcmp(option, "HOP_BY_HOP")) {
+			cp_cur = ipv6_hbh_writer(fp, cp_cur);
+		}
+		else {
+			fprintf(stderr, "Unknown IPv6 extention header option\n");
+			goto err;
+		}
+
+		if (cp_cur == NULL)
+			goto err;
+	}
+#if 0
 	pkt->ver_tc_fw |= sp_pd->ipv6_version << 28;
 	pkt->ver_tc_fw |= sp_pd->ipv6_traffic_class << 20;
 	pkt->ver_tc_fw |= sp_pd->ipv6_flow_label;
@@ -36,8 +194,9 @@ char* pgen_ipv6_hdr_writer(struct packet_data *sp_pd, char *cp_cur) {
 		fprintf(stderr, "IPV6: dst_ip conversion failed\n");
 		goto err;
 	}
+#endif
 
-	return (cp_cur + sizeof(struct ipv6_packet));
+	return cp_cur;
 
 err:
 	return NULL;
