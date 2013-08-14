74422dd7acd93e9528449e94efc031a27810897c
kumaran127 <kumaran.4353@gmail.com>
2013-08-14 19:28:20 +0530

    Clean-up done for help.c

--- help.c
+++ help.c
@@ -1,24 +1,54 @@
 #include "pgen.h"
 
+/**
+ * @Description
+ * 		Tells how to use pgen while you are using it wrong
+ */
 void usage() {
 	PGEN_INFO("Usage: pgen [conf_file]\n");
 }
 
-int pgen_strcmp(const char *s1, const char *s2) {
+/**
+ * @param	s1	Destination pointer
+ * @param	s2	Source pointer
+ *
+ * @returns
+ *			0	Success
+ *			!0	Error
+ *
+ * @Description
+ * 		As strcmp is vulnurable, we are having a wrapper function over strncmp.
+ * 	But anyway it is not used much in this project (ridiculous!).
+ */
+int32_t pgen_strcmp(const char *s1, const char *s2) {
 	return strncmp(s1, s2, strlen(s2) + 1);
 }
 
-int send_packet(const char *if_name, const char *dst_mac, const char *cp_buff,
-		const int buff_size) {
-	int sockfd;
+/**
+ * @param	if_name		name of the interface by which the packet has to be
+ *						sent.
+ * @param	dst_mac		Mac address of the destination node
+ * @param	cp_buff		The packet buffer
+ * @param	buff_size	Size of the packet buffer
+ *
+ * @return
+ *			0		Success
+ *			-1		Failure
+ *
+ * @Description
+ *		Here comes the end. This function puts the packet buffer into wire.
+ */
+int32_t send_packet(const char *if_name, const char *dst_mac, const char *cp_buff,
+		const int32_t buff_size) {
+	int32_t sockfd;
 	struct sockaddr_ll s_sock_addr;
 	struct ifreq s_if_idx;
 	struct ifreq s_if_mac;
 
-	/* Get the RAW socket */
+	/* Get the RAW socket. Must have root permission */
     sockfd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
     if (sockfd < 0) {
-		PGEN_ERROR("socket", errno);
+		PGEN_ERROR("socket creation failed", errno);
         goto err;
     }
 
@@ -26,7 +56,7 @@ int send_packet(const char *if_name, const char *dst_mac, const char *cp_buff,
     memset(&s_if_idx, 0, sizeof (struct ifreq));
     strcpy(s_if_idx.ifr_name, if_name);
     if (ioctl(sockfd, SIOCGIFINDEX, &s_if_idx) < 0) {
-		PGEN_ERROR("ioctl", errno);
+		PGEN_ERROR("ioctl to get if index failed", errno);
         goto err;
     }
 
@@ -34,7 +64,7 @@ int send_packet(const char *if_name, const char *dst_mac, const char *cp_buff,
     memset(&s_if_mac, 0, sizeof(struct ifreq));
     strcpy(s_if_mac.ifr_name, if_name);
     if (ioctl(sockfd, SIOCGIFHWADDR, &s_if_mac) < 0) {
-		PGEN_ERROR("ioctl", errno);
+		PGEN_ERROR("ioctl to get if mac failed", errno);
         goto err;
     }
 
@@ -46,21 +76,36 @@ int send_packet(const char *if_name, const char *dst_mac, const char *cp_buff,
         goto err;
     }
 
+	/* Ok. Now send it */
 	if (sendto(sockfd, cp_buff, buff_size, 0,
                 (struct sockaddr *)&s_sock_addr,
                 sizeof(struct sockaddr_ll)) < 0) {
-		PGEN_ERROR("sendto", errno);
+		PGEN_ERROR("sendto failed", errno);
         goto err;
     }
 	return 0;
+
 err:
 	return -1;
 }
 
-int mac_writer(char *dst, const char *src) {
-    int seg = 0;
+/**
+ * @param	dst		Destination pointer where the resulting mac address
+ *					will be stored
+ * @param	src		Source character pointer in which mac address to be
+ *					converted is passed
+ *
+ * @return
+ *			0		Success
+ *			-1		Failure
+ *
+ * @Description
+ *			Converts mac from string form into binary form and stores in dst
+ */
+int32_t mac_writer(char *dst, const char *src) {
+    int32_t seg = 0;
     char ind;
-    int i, j = 0;
+    int32_t i, j = 0;
 
     for (i = 0; i < 17; i++) {
         ind = src[i];
@@ -74,27 +119,42 @@ int mac_writer(char *dst, const char *src) {
             dst[j++] = (unsigned char) seg;
             seg = 0;
         }
-        else
+        else {
+			PGEN_INFO("mac_writer returns error");
             return -1;
+		}
     }
 	dst[j] = (unsigned char) seg;
     return 0;
 }
 
-int ip4_writer(char *dst, const char *src) {
+/**
+ * @param	dst		destination pointer where binary IPv4 address will be
+ *					stored
+ * @param	src		source pointer in which string form of IPv4 address is
+ *					passed
+ *
+ * @return
+ *			0		Success
+ *			-1		Error
+ *
+ * @Description
+ *		This function converts string form of IPv4 address into binary form
+ * and stores that in dst. It is simply a wrapper over inet_pton
+ */
+int32_t ip4_writer(char *dst, const char *src) {
 
-	struct in_addr *sin_addr = (struct in_addr *)dst;
-	if (inet_pton(AF_INET, src, sin_addr) == 0) {
+	if (inet_pton(AF_INET, src, dst) == 0) {
 		PGEN_INFO("Invalid IP address");
 		return -1;
 	}
 	return 0;
 
-	/* Could be helpful later */
+	/* Could be helpful in future */
 #if 0
-    int seg = 0;
+    int32_t seg = 0;
     char ind;
-    int i, j = 0;
+    int32_t i, j = 0;
     
     for (i = 0; i < 15; i++) {
         ind = src[i];
@@ -115,9 +175,22 @@ int ip4_writer(char *dst, const char *src) {
 #endif
 }
 
-int ip6_writer(char *dst, const char *src) {
-	struct in6_addr *sin6_addr = (struct in6_addr *)dst;
-	if (inet_pton(AF_INET6, src, sin6_addr) == 0) {
+/**
+ * @param	dst		Destination pointer where IPv6 binary form will be stored
+ * @param	src		Source pointer in which string of IPv6 address is passed
+ *					to this function
+ *
+ * @return
+ *			0		Success
+ *			-1		Error
+ *
+ * @Description
+ *		It converts IPv6 address in string form into binary form and stores it
+ * in dst pointer. Like ip4_writer, it is also a wrapper over inet_pton
+ */
+int32_t ip6_writer(char *dst, const char *src) {
+
+	if (inet_pton(AF_INET6, src, dst) == 0) {
 		PGEN_INFO("Invalid IPv6 address");
 		return -1;
 	}
@@ -126,8 +199,8 @@ int ip6_writer(char *dst, const char *src) {
 	/* Could be helpful in future */
 #if 0
 	char temp[IPV6_ADDR_MAX_LEN] = {0}, c;
-	unsigned int seg = 0;
-	int i;
+	unsigned int32_t seg = 0;
+	int32_t i;
 
 	if (!src || !dst)
 		goto err;
@@ -162,15 +235,34 @@ err:
 #endif
 }
 
-int ip6_prefix_writer(char *dst, char *value) {
+/**
+ * @param	dst		Destination pointer where binary form of IPv6 prefix will
+ *					be stored
+ * @param	src		Source pointer in which string form of IPv6 address is
+ *					supplied.
+ *
+ * @return
+ *			0		Success
+ *			-1		Error
+ *
+ * @Description
+ *		This function converts the IPv6 prefix from string form to binary form
+ * and stores that in dst. As the prefix is not a valid IPv6 address, we could
+ * not use inet_pton :(
+ *
+ * @Action_item
+ *		This function could be made much wiser to allow user to give IPv6
+ * address in short form
+ */
+int32_t ip6_prefix_writer(char *dst, char *value) {
 	uint32_t i, one_char, two_char, pos = 1;
 	char tmp[IPV6_ADDR_MAX_LEN] = {0};
 
+	/* Fist expand the IPv6 prefix */
 	if (ip6_expander(tmp, value))
 		goto err;
 
-	printf("%s\n", tmp);
-
+	/* Convert character by character */
 	for (i = 0; i < strlen(tmp); i++) {
 		if ((tmp[i] >= 'a') && (tmp[i] <= 'f'))
 			one_char = tmp[i] - 'a' + 10;
@@ -199,21 +291,32 @@ int ip6_prefix_writer(char *dst, char *value) {
 	return 0;
 
 err:
+	PGEN_INFO("Invalid IPv6 prefix");
 	return -1;
 }
 
 /**
- * This is used to expand the IPv6 shorter form into its expanded form.
+ * @param	dst		Destination pointer where expanded IPv6 address will be
+ *					stored
+ * @param	src		Source pointer in which IPv6 shorter form is passed
+ *
+ * @return
+ *			0		Success
+ *			-1		Error
+ *
+ * @Description
+ * 		This is used to expand the IPv6 address shorter form into its expanded
+ * form.
  */
-int ip6_expander(char *dst, const char *src) {
-	int len, rem, dots;
-	int i, j = 0, k = 0, nxt_col;
+int32_t ip6_expander(char *dst, const char *src) {
+	int32_t len, rem, dots;
+	int32_t i, j = 0, k = 0, nxt_col;
 	char intr[IPV6_ADDR_MAX_LEN], temp[5];
 
 	if (!dst || !src)
 		goto err;
 
-	/* :: is the shortest form of IPv6 address - All zero IPv6 addr */
+	/* :: is the shortest IPv6 address - All zero IPv6 addr */
 	if (!strcmp(src, "::")) {
 		strcpy(dst, "0000:0000:0000:0000:0000:0000:0000:0000");
 		return 0;
@@ -275,15 +378,27 @@ int ip6_expander(char *dst, const char *src) {
 	return 0;
 
 err:
+	PGEN_INFO("Invalid IPv6 address/prefix");
 	return -1;
 }
 
 /**
- * To parse the configuration file.
- * It reads one line at a time and parses the option & value
- * and returns them to the caller with the pointers supplied.
+ * @param	fp		file pointer to the configuration file
+ * @param	option	The option will be stored in this pointer and passed back
+ *					to the callee.
+ * @param	value	The value for the option will be stored in this pointer and
+ *					passed back to callee.
+ *
+ * @return
+ *			0		Success
+ *			-1		Error
+ *
+ * @Description
+ * 		To parse the configuration file. This function reads the line where
+ * cursur is currently present and parses it. The option and its corresponding
+ * value will be stored in option and value pointer respectively.
  */
-int pgen_parse_option(FILE *fp, char *option, char *value) {
+int32_t pgen_parse_option(FILE *fp, char *option, char *value) {
 	char line[MAX_LINE_LENGTH];
 	char *c = NULL, *op, *val;
 	char ch;
@@ -338,6 +453,7 @@ next:
 
 		return 0; 
     }
+	/* End of file is reached. This is an error case */
 	return EOF;
 
 err:
@@ -347,28 +463,58 @@ err:
 }
 
 /**
- * Converts character numbers into their decimal values.
+ * @param	i		Destination pointer where converted decimal value will
+ *					be stored.
+ * @param	c		Source pointer in which string form of the the number
+ *					is passed by
+ *
+ * @return
+ *			0		Success
+ *			-ERRNO	Error
+ *
+ * @Description
+ * 		Converts character numbers into their decimal values.
  * As it depends on strtol, this function doesn't bother about
- * rear non-number characters.
+ * rear non-number characters. It is just a wrapper over strtol
  */
-int pgen_store_dec(int *i, const char *c) {
+int32_t pgen_store_dec(int32_t *i, const char *c) {
 	errno = 0;
 	*i = strtol(c, NULL, 10);
-	if (errno)
-		return -1;
+	if (errno) {
+		PGEN_INFO("Decimal conversion failed");
+		return -errno;
+	}
 
 	return 0;
 }
 
-int validate_if(const char *if_name) {
+/**
+ * @param	if_name		Name of the network interface which should be verified
+ *
+ * @return
+ *			0			Success
+ *			-1			Error
+ *
+ * @Description
+ *			Verifies whether the passed network interface is a legitimate one.
+ * It just checks whether the interface is available and UP.
+ */
+int32_t validate_if(const char *if_name) {
     struct ifreq req;
-    int sockfd;
-    int ret;
+    int32_t sockfd;
+    int32_t ret;
 
+	/* Null check */
+	if (!if_name) {
+		PGEN_INFO("Argument is NULL");
+		goto err;
+	}
+
+	/* To get interface's index and flags, we need a socket */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
 		PGEN_ERROR("Socket creation failed", errno);
-        return -1;
+		goto err;
     }
 
     memset(&req, 0, sizeof(struct ifreq));
